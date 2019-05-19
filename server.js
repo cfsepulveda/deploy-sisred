eea09c4d401556ecd783cc9204a9a7e2914a0021
cfsepulveda <42418118+cfsepulveda@users.noreply.github.com>
2019-05-19 15:37:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,9 @@ res.sendFile(path.join(__dirname+'/dist/angular/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
